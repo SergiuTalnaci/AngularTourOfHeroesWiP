5171d7638a0a31f11a1cad27944ed69bd958dd4d
sergiu230 <sergiu230@gmail.com>
2017-04-17 11:24:51 +0200

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,46 @@
-﻿import { NgModule }      from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { HeroService } from './services/hero.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [
-    AppComponent,
-    HeroDetailComponent,
-    HeroesComponent
-  ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {
+                path: 'heroes',
+                component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            }
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
